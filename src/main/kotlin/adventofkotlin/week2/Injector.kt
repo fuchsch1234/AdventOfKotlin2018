db80e7396f647d4133cd4d320ecc87d3bcb1af52
cf <abtjh5q@hmamail.com>
2018-12-08 06:56:57 +0100

    Hide typeRegistry member, just an implementation detail

--- src/main/kotlin/adventofkotlin/week2/Injector.kt
+++ src/main/kotlin/adventofkotlin/week2/Injector.kt
@@ -3,11 +3,10 @@ package adventofkotlin.week2
 import java.lang.Exception
 import kotlin.reflect.KProperty
 
-@Suppress("UNCHECKED_CAST")
 inline fun <reified T> get(): T {
     val typeName = T::class.qualifiedName ?: T::class.toString()
-    val constructor = Injector.typeRegistry[Injector.Key(typeName)]
-    return constructor?.let { (it as ()->T).invoke() }
+    val constructor = Injector.getConstructor<T>(Injector.Key(typeName))
+    return constructor?.invoke()
         ?: throw UnsatisfiableDependency("Cannot create object of type $typeName")
 }
 
@@ -20,12 +19,11 @@ class UnsatisfiableDependency(what: String) : Exception(what)
 
 class DelegateProvider<T>(private val typeName: String) {
 
-    @Suppress("UNCHECKED_CAST")
     operator fun provideDelegate(thisref: Any?, property: KProperty<*>): Delegate<T> {
         // Try to find constructor for type and property name, or general constructor for type
-        val constructor = Injector.typeRegistry[Injector.Key(typeName, property.name)]
-            ?: Injector.typeRegistry[Injector.Key(typeName)]
-        return constructor?.let { Delegate(it as ()->T) }
+        val constructor = Injector.getConstructor<T>(Injector.Key(typeName, property.name))
+            ?: Injector.getConstructor<T>(Injector.Key(typeName))
+        return constructor?.let { Delegate(it) }
             ?: throw UnsatisfiableDependency("Cannot create object of type $typeName")
     }
 
@@ -45,7 +43,7 @@ object Injector {
 
     data class Key(val typeName: String, val varName: String? = null)
 
-    val typeRegistry = emptyMap<Key, ()->Any?>().toMutableMap()
+    private val typeRegistry = emptyMap<Key, ()->Any?>().toMutableMap()
 
     inline fun <reified T> bind(varName: String? = null): Binder<T> {
         val typeName = T::class.qualifiedName ?: T::class.toString()
@@ -55,6 +53,9 @@ object Injector {
     fun providing(f: Injector.()->Unit) {
         this.f()
     }
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T> getConstructor(key: Key): (() -> T)? = typeRegistry[key] as (() -> T)?
 
     fun reset() {
         typeRegistry.clear()
